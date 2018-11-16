273e9ff58b5037b3e553164f6984554ed4dbb616
devmaca <m.a.c.a-964@hotmail.com>
2018-11-15 21:19:40 -0400

    de grober primer version

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace prueba2\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\facades\schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
